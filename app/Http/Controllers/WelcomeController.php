95bf9d4f810a839a754faa27f3c3fe547f6a66cb
shivam tiwari <bbtshivam@gmail.com>
2023-05-30 21:07:16 +0530

    new admin setup

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class WelcomeController extends Controller
+{
+    
+    private static $bypass_url = [];
+
+    public function __construct() {
+        
+    }
+}
